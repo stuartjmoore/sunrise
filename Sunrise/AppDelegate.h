c37d5599553540a2d7505bd248dccd591ba7db9e
stuartjmoore <stuartjmoore@gmail.com>
2014-08-26 13:24:53 -0600

    Initial Commit

---
+++ Sunrise/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Sunrise
+//
+//  Created by Stuart Moore on 8/26/14.
+//  Copyright (c) 2014 Stuart J. Moore. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Sunrise/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Sunrise
+//
+//  Created by Stuart Moore on 8/26/14.
+//  Copyright (c) 2014 Stuart J. Moore. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
